8297d0bcaa322629bccb32f229c04e1210228352
Bobbar <bobbar@gmail.com>
2018-05-02 15:28:56 -0400

    Rename database library folder.

--- Databases/Properties/AssemblyInfo.cs
+++ Databases/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AssetDatabase")]
+[assembly: AssemblyTitle("Databases")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AssetDatabase")]
+[assembly: AssemblyProduct("Databases")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
