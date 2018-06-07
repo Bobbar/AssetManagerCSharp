48460f5e2877a830e40054178c0cd71ebe4fb3b4
Bobbar <bobbar@gmail.com>
2018-06-07 15:16:36 -0400

    Change deployment modules local store path to user profile.

--- AssetManager/GlobalObjects/Paths.cs
+++ AssetManager/GlobalObjects/Paths.cs
@@ -1,21 +1,18 @@
+using AssetManager.Data;
 using System;
 using System.Deployment.Application;
-using AssetManager.Data;
-using System.IO;
 
 namespace AssetManager
 {
-    static class Paths
+    internal static class Paths
     {
-
         //Application paths
-
-
         public static readonly string AppDir = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\AssetManager\";
+
         public static readonly string PsExecPath = AppDomain.CurrentDomain.BaseDirectory + @"Tools\PsExec\PsExec.exe";
-        public static readonly string LocalModulesStore = AppDomain.CurrentDomain.BaseDirectory + @"\DeploymentModulesStore\";
-        public static readonly string RemoteModulesStore   = @" \\core.co.fairfield.oh.us\dfs1\fcdd\files\QA\Asset Management\Asset Manager\DeploymentModules\";
-       
+        public static readonly string LocalModulesStore = AppDir + @"\DeploymentModulesStore\";
+        public static readonly string RemoteModulesStore = @" \\core.co.fairfield.oh.us\dfs1\fcdd\files\QA\Asset Management\Asset Manager\DeploymentModules\";
+
         public const string PsExecTempDir = @"C:\Temp\";
         public const string PsExecTempPath = @"C:\Temp\PsExec.exe";
 
@@ -35,10 +32,11 @@ namespace AssetManager
             get { return AppDir + @"SQLiteCache\" + SQLiteDBName; }
         }
 
-
         public static readonly string SQLiteDir = AppDir + @"SQLiteCache\";
+
         //Gatekeeper package paths
         public const string GKRemoteDir = @"\PSi\Gatekeeper\";
+
         public const string GKLocalInstallDir = @"C:\PSi\Gatekeeper";
         public const string GKPackFileName = "GatekeeperPack.gz";
         public const string GKPackHashName = "hash.md5";
