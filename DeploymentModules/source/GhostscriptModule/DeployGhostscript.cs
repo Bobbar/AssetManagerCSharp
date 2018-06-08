e6de5a4715b6696b9a823d37959151011922db9f
Bobbar <bobbar@gmail.com>
2018-06-08 10:11:45 -0400

    Added deploy module for Ghostscript PDF printer.

---
+++ DeploymentModules/source/GhostscriptModule/DeployGhostscript.cs
@@ -0,0 +1,49 @@
+﻿using DeploymentAssemblies;
+using System.Threading.Tasks;
+
+namespace GhostscriptModule
+{
+    public class DeployGhostscript : DeploymentAssemblies.IDeployment
+    {
+        private DeploymentAssemblies.IDeploymentUI deploy;
+
+        public string DeploymentName
+        {
+            get
+            {
+                return "Ghostscript PDF";
+            }
+        }
+
+        public void InitUI(IDeploymentUI ui)
+        {
+            deploy = ui;
+            deploy.UsePsExec();
+        }
+
+        public async Task<bool> DeployToDevice()
+        {
+            if (await deploy.SimplePSExecCommand(deploy.GetString("ghostscript_driver"), "Ghostscript PDF Driver Install"))
+            {
+                deploy.LogMessage("Ghostscript Driver Installed.");
+            }
+            else
+            {
+                deploy.LogMessage("Ghostscript Driver Install Failed!");
+                return false;
+            }
+
+            if (await deploy.SimplePSExecCommand(deploy.GetString("ghostscript_install"), "Ghostscript PDF Printer Install"))
+            {
+                deploy.LogMessage("Ghostscript PDF Printer Installed.");
+            }
+            else
+            {
+                deploy.LogMessage("Ghostscript PDF Printer Install Failed!");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
